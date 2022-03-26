b247382545998dd23a63c7f4728b55fa7c69ba2d
Gabriel Caetano de Oliveira <caetanoog@outlook.com>
2022-03-26 01:16:11 -0300

    Primeiro commit

---
+++ routes/jobs.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+//Rota de teste
+router.get('/test', (req,res)=>{
+    res.send('deu certo');
+});
+
+//detalhe da vaga -> view/1, view/2
+router.get('/view/:id' ,(req,res)=> Job.findOne({
+    where: {id: req.params.id}
+}).then(job=> {
+    res.render('view', {
+            job
+        });
+    }).catch(erro => console.log(erro)));
+
+//form da rota de envio
+router.get('/add', (req,res)=> {
+    res.render('add');
+})
+
+//Add job via post
+router.post('/add', (req,res) =>{
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    //Insert
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(erro => console.log(erro));
+
+});
+
+module.exports = router
